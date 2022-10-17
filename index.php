cc54122296e6859b2af6649f2dc662ac80cd6161
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-17 17:23:03 +0530

    ss

--- index.php
+++ index.php
@@ -38,10 +38,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <script src="js/jsddd.js"></script>
 
 
-<!-- <link rel="stylesheet" href="wp-content/themes/clove-child/css/newhome-style.css" type="text/css">
+<link rel="stylesheet" href="wp-content/themes/clove-child/css/newhome-style.css" type="text/css">
 	
 <script defer src="wp-includes/js/jquery/jqueryb8ff.js?ver=1.12.4"></script>
-<script defer src="wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1"></script> -->
+<script defer src="wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1"></script>
 <script async id="sourcecode">
 </script>
 <script defer async src="https://www.googletagmanager.com/gtag/js?id=AW-958029923"></script>
