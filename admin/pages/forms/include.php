8406deb4897fa1376630729019fc08da0ed91b14
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-17 15:55:42 +0530

    ssss

--- admin/pages/forms/include.php
+++ admin/pages/forms/include.php
@@ -1,22 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "GoBestDentist";
-$password = "Admin@123";
-$dbname = "Gobestdentist";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-
-
-// $servername = "151.106.124.51";
-// $username = "u188140722_dentist";
+// $servername = "localhost";
+// $username = "GoBestDentist";
 // $password = "Admin@123";
-// $dbname = "u188140722_dentist";
+// $dbname = "Gobestdentist";
 
 // // Create connection
 // $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -24,5 +10,19 @@ if (!$conn) {
 // if (!$conn) {
 //   die("Connection failed: " . mysqli_connect_error());
 // }
+
+
+
+$servername = "151.106.124.51";
+$username = "u188140722_dentist";
+$password = "Admin@123";
+$dbname = "u188140722_dentist";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
--- include/config.php
+++ include/config.php
@@ -1,3 +1,7 @@
+<!-- <?php
+// $conn = new mysqli("localhost","GoBestDentist","Admin@123","Gobestdentist");
+?> -->
+
 <?php
-$conn = new mysqli("localhost","GoBestDentist","Admin@123","Gobestdentist");
+$conn = new mysqli("151.106.124.51","u188140722_dentist","Admin@123","u188140722_dentist");
 ?>
