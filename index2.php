e5f4221e096e4241b112e347e52962c70e2d73a9
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-27 16:03:16 +0530

    ssss

---
+++ index2.php
@@ -0,0 +1,338 @@
+<?php
+include("include/config.php") ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="stylenew.css">
+
+</head>
+
+<body>
+
+    <div class="">
+
+        <!-- TREATMENTS -->
+        <section class="treatment-sec">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2 class="treatmentHeading">SYMPTOMS & TREATMENTS</h2>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/filling-icon.webp">
+                            <p class="text-center tratmentTitle">Dental Fillings</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/root-canal-icon.webp">
+                            <p class="text-center tratmentTitle">Root Canal Treatment</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/teeth-removal-icon.webp">
+                            <p class="text-center tratmentTitle">Wisdom Teeth Removal</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/braces-aligners-icon.webp">
+                            <p class="text-center tratmentTitle">Braces & Aligners</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/dental-implant-icon.webp">
+                            <p class="text-center tratmentTitle">Dental Implants</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/dentures-icon.webp">
+                            <p class="text-center tratmentTitle">Dentures</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/crowns-bridges-icon.webp">
+                            <p class="text-center tratmentTitle">Bridges & Crowns</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/childrens-dentistry-icon.webp">
+                            <p class="text-center tratmentTitle">Kids Dentistry</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/smile-makeover-icon.webp">
+                            <p class="text-center tratmentTitle">Smile Makeover</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/teeth-whitening-icon.webp">
+                            <p class="text-center tratmentTitle">Teeth Whitening</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/mouth-ulcers-icon.webp">
+                            <p class="text-center tratmentTitle">Mouth Ulcers</p>
+                        </a>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 treatmentsContainer">
+                        <a href="" class="treatments">
+                            <img alt="nearest dental clinic" class="centerImg"
+                                src="wp-content/themes/clove-child/images/read-more-icon.webp">
+                            <p class="text-center tratmentTitle">View More</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <!-- COMPARISON IMAGE SECTION -->
+        <section class="slider-sec">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-12  mt-3 " id="sliderCompImg">
+                        <div class="img-comp-container">
+                            <div class="img-comp-img">
+                                <img style="display: block; vertical-align: middle;" class="imgComp"
+                                    src="wp-content/uploads/2020/10/safety1-min.webp" width="500px" height="500px">
+                            </div>
+                            <div class="img-comp-img img-comp-overlay">
+                                <img style="display: block; vertical-align: middle;" class="imgComp"
+                                    src="wp-content/uploads/2020/10/safety2-min.webp" width="500px" height="500px">
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-12">
+
+                        <div class="about-info py-3" data-aos="fade-up" data-aos-duration="2000">
+                            <h5 class="mb-3">WHO WE ARE</h5>
+                            <h2 class="mb-3">Multi-Specialty Dental Clinic In Pimpri-Chinchwad, Pune</h2>
+                            <p>If you need to know is there a <strong>Best Dentist in pimpri chinchwad</strong> or
+                                <strong>Best Dental Clinic in pimpri chinchwad</strong>. Yes, there is. GoBest Dentist
+                                is a modern, hi-tech, and ISO certified chain of multi-specialty dental clinic in Pune,
+                                India. Headed by Dr. Sana Mokashi it has a team of super specialists that is dedicated
+                                towards delivering all dental treatment under one roof. We strive to provide high
+                                quality, personalized dental care in a relaxing, comfortable, and safe environment.
+                                Lifelong relationships are created with our patients based on trust and honesty.
+                            </p>
+                            <div class="row about-point">
+                                <div class="col-lg-4 col-6" data-aos="fade-up" data-aos-duration="500">
+                                    <div class="point" style="text-align:center;"><a href="#." class="btn">Affordable
+                                            <br> Dentists</a></div>
+                                </div>
+                                <div class="col-lg-4 col-6" data-aos="fade-up" data-aos-duration="700">
+                                    <div class="point" style="text-align:center;"><a href="#." class="btn">Advanced <br>
+                                            Techniques</a></div>
+                                </div>
+                                <div class="col-lg-4 col-6" data-aos="fade-up" data-aos-duration="900">
+                                    <div class="point" style="text-align:center;"><a href="#." class="btn">Hygiene
+                                            Safety <br> priority</a></div>
+                                </div>
+                                <div class="col-lg-4 col-6" data-aos="fade-up" data-aos-duration="1200">
+                                    <div class="point" style="text-align:center;"><a href="#." class="btn">Advanced <br>
+                                            Instruments</a></div>
+                                </div>
+                                <div class="col-lg-4 col-6" data-aos="fade-up" data-aos-duration="1500">
+                                    <div class="point" style="text-align:center;"><a href="#."
+                                            class="btn">Multispecialist <br> Dentists</a></div>
+                                </div>
+                                <div class="col-lg-4 col-6" data-aos="fade-up" data-aos-duration="1800">
+                                    <div class="point" style="text-align:center;"><a href="#." class="btn">Professional
+                                            <br> Staff</a></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- KNOW MORE -->
+        <section class="knowmore-sec">
+            <div class="container text-center">
+                <h2 class=" offset-xl-2">Know More About Our Success Stories</h2>
+                <div class="row">
+                    <div class="col-lg-2 offset-lg-1 col-12 my-lg-0">
+                        <img src="images/ISO-CERTIFIED-logo.webp" alt="ISO" class="img-fluid image2">
+                    </div>
+                    <div class="col-lg-2 col-sm-6 col-5 shiva">
+                        <div id="shiva"><span class="counter count">30,000</span> <span class="plus">+</span>
+                            <p>Happy Patients</p>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-2 col-sm-6 col-5 shiva">
+                        <div id="shiva"><span class="counter count">30</span> <span class="plus">+</span>
+                            <p>Experts Doctors</p>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-2 col-sm-6 col-5 shiva">
+                        <div id="shiva"><span class="counter count">100</span> <span class="plus">+</span>
+                            <p>Treatments</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-sm-6 col-5 shiva">
+                        <div id="shiva"><span class="counter count">4</span> <span class="plus">+</span>
+                            <p>Branches In Pune</p>
+                        </div>
+                    </div>
+                    <div class="offset-lg-1"></div>
+                </div>
+            </div>
+        </section>
+
+
+
+        <!--video-->
+        <section class="patient_speaks">
+            <div class="container">
+                <div class="heading">
+                    <h2>Patient Speaks</h2>
+                </div>
+                <div class="patients_speaks_wrap">
+                    <div class="patient_speak_videos row">
+                        <?php
+              $sql = "SELECT * FROM review WHERE is_delete = '0' LIMIT 4";
+              $data = mysqli_query($conn, $sql);
+              foreach($data as $d){
+          ?>
+                        <!-- <div class="item item1 ">
+                            <div class="video-box">
+                                <iframe width="260" height="320" id="<?php echo $d['id']; ?>"
+                                    src="https://www.youtube.com/embed/<?php echo $d['link']; ?>?rel=0&wmode=Opaque&enablejsapi=1;showinfo=0;controls=0"
+                                    class="iframe-video yt_players" title="YouTube video player" frameborder="0"
+                                    allowfullscreen></iframe>
+                            </div>
+                        </div> -->
+                        <?php } ?>
+                        <div class="clearfix"></div>
+                        <div style="text-align:center;">
+                            <a href="testimonials.php" class="btn view_more">View More</a>
+                        </div>
+                    </div>
+                </div><!-- patients_speaks_wrap end here -->
+            </div>
+        </section>
+        <!--video-->
+
+
+    </div>
+    <!-- container -->
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <script>
+    function initComparisons() {
+        var x, i;
+        x = document.getElementsByClassName("img-comp-overlay");
+        for (i = 0; i < x.length; i++) {
+
+            compareImages(x[i]);
+        }
+
+        function compareImages(img) {
+            var slider, img, clicked = 0,
+                w, h;
+            w = img.offsetWidth;
+            h = img.offsetHeight;
+            img.style.width = (w / 2) + "px";
+            /*create slider:*/
+            slider = document.createElement("img");
+            slider.setAttribute("class", "img-comp-slider");
+            slider.setAttribute("src", "slider.png")
+
+            /*insert slider*/
+            img.parentElement.insertBefore(slider, img);
+            slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
+            slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
+            slider.addEventListener("mousedown", slideReady);
+            window.addEventListener("mouseup", slideFinish);
+            slider.addEventListener("touchstart", slideReady);
+            window.addEventListener("touchend", slideFinish);
+
+            function slideReady(e) {
+                e.preventDefault();
+                clicked = 1;
+                window.addEventListener("mousemove", slideMove);
+                window.addEventListener("touchmove", slideMove);
+            }
+
+            function slideFinish() {
+                clicked = 0;
+            }
+
+            function slideMove(e) {
+                var pos;
+                if (clicked == 0) return false;
+                pos = getCursorPos(e)
+                if (pos < 0) pos = 0;
+                if (pos > w) pos = w;
+                slide(pos);
+            }
+
+            function getCursorPos(e) {
+                var a, x = 0;
+                e = (e.changedTouches) ? e.changedTouches[0] : e;
+                a = img.getBoundingClientRect();
+                x = e.pageX - a.left;
+                x = x - window.pageXOffset;
+                return x;
+            }
+
+            function slide(x) {
+                img.style.width = x + "px";
+                slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+            }
+        }
+    }
+    </script>
+
+    <script>
+    initComparisons();
+    </script>
+
+</body>
+
+</html>
