6f9065462ba321cafc913c09a48b98b3146d1db6
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-28 15:25:46 +0530

    ssss

--- index.php
+++ index.php
@@ -863,18 +863,15 @@ include("include/config.php") ?>
 
 
 
+<?php include("include/footer1.php") ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-
-
-    <script defer src="wp-content/themes/clove-child/js/style.js"></script>
-
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="js/jsddd.js"></script>
+    
 
 
 
--- js/jsddd.js
+++ js/jsddd.js
@@ -76,6 +76,54 @@ $(document).ready(function(){
                  }
                  else{return false;}
             });
+
+            
 });
 
+
+(function($){
+$('.banner-form').click(function(){
+   //$('#frm_type').val('value','1000');
+   $('#frm_type').remove();
+   input = jQuery('<input type="hidden" name="form_type" id="frm_types" value="website banner econsult">');
+   $('#for_banner').append(input);
+   
+   
+   
+   });
+   jQuery(".toggle-menu").click(function(){
+                                //alert("helo");
+         jQuery(".navigation").slideToggle("slow");
+         jQuery(this).toggleClass("active");
+         jQuery("body").toggleClass("highlight");
+         jQuery('.sub-menu').slideUp();
+         jQuery('.mobile-plus').removeClass('active');
+      }); 
+   jQuery(".navigation li").find("ul").after("<span class='mobile-plus' ></span>");
+      jQuery(".mobile-plus").click(function(){
+         jQuery(this).parent().siblings().find('.sub-menu').slideUp();
+         jQuery(this).parent().siblings().find('.mobile-plus').removeClass('active');        
+         jQuery(this).parent().find('.sub-menu').slideToggle();            
+         jQuery(this).toggleClass("active");
+      });
+   
+      jQuery(window).resize(function(){
+         var wwindow = jQuery(window).width();
+         if (wwindow >= 1025) {           
+            jQuery(".toggle-menu").removeClass("active");
+            jQuery(".nav-bar .navigation").removeAttr("style");
+            jQuery(".nav-bar .navigation ul ul.sub-menu").removeAttr("style");
+         }
+      });
+   jQuery(".header-main .toggle-icon").click(function(){
+         jQuery(".side-menu").addClass("slide-right");
+         jQuery('.transparent-bg').fadeIn();
+         jQuery("body").addClass("no-scroll");
+      });
+      jQuery(".side-menu-close").click(function(){
+         jQuery(".side-menu").removeClass("slide-right");
+         jQuery('.transparent-bg').fadeOut();
+         jQuery("body").removeClass("no-scroll");
+      });
+   }(jQuery));
 
