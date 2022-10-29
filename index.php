ceb417ca8374986cad45d25e0f555ddc81345331
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-29 11:37:29 +0530

    ssss

--- index.php
+++ index.php
@@ -880,10 +880,6 @@ include("include/config.php") ?>
 
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
     <script src="js/jsddd.js"></script>
 
