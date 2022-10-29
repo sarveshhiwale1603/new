f543049c04677931112000e5598131dd5b115376
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-29 11:19:36 +0530

    sss

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include("include/config.php") ?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <link rel="shortcut icon" href="wp-content\themes\clove\images\favicon.png" type="image/x-icon">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
