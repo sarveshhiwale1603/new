4cd7c416b5b3317f8ec67e3a9eb3f10bc422221a
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-29 16:27:28 +0530

    sa

--- index.php
+++ index.php
@@ -14,11 +14,12 @@ include("include/config.php") ?>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        
     <link href="wp-content/themes/clove/css/fa.css" rel="stylesheet">
     <link rel="stylesheet" href="stylenew.css">
     <link rel="stylesheet" href="newhome.css">
     <!-- <link rel="stylesheet" href="wp-content/themes/clove-child/css/newhome-style.css" type="text/css"> -->
-    
+     
     <style>
     .toptext{
         text-align:center;
