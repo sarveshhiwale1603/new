49f8a3c5c3f8d263b6501d7064f68f23005e37bd
sarveshhiwale1603 <sarveshhiwale07@gamil.com>
2022-10-29 16:06:48 +0530

    asd

--- index.php
+++ index.php
@@ -25,6 +25,10 @@ include("include/config.php") ?>
             margin-right: 20px !important;
         }
     }
+
+    .button-50:hover{
+        color:#0704b4;
+    }
     </style>
 
 </head>
@@ -847,6 +851,10 @@ include("include/config.php") ?>
     <a href="tel:+917264889986;" class="phone-btn wow slideInRight ui-btn ui-shadow ui-corner-all " title="Call Us"
         style="color:#ffffff;"><i class="fa fa-phone"></i></a>
 
+        
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
     <script src="js/jsddd.js"></script>
     <!-- <script defer src="wp-content/themes/clove-child/js/style.js"></script> -->
